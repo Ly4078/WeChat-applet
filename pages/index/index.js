66fd9bf60e094bf0ccea62957082f51c959568bb
yangfeng <1138391635@qq.com>
2018-03-13 20:05:02 +0800

    合并完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,5 @@
 //index.js 
-import Api from '/../../utils/config/api.js'; 
+import Api from '/../../utils/config/api.js';
 import { GLOBAL_API_DOMAIN } from '/../../utils/config/config.js';
 var postsData = require('/../../data/posts-data.js')
 const app = getApp()
@@ -8,15 +8,15 @@ Page({
   data: {
     _build_url: GLOBAL_API_DOMAIN,
     city: "",
-    carousel:[],  //轮播图
-    business:[], //商家列表，推荐餐厅
-    actlist:[],  //热门活动
-    hotlive:[],  //热门直播
-    food:[],   //美食墙
+    carousel: [],  //轮播图
+    business: [], //商家列表，推荐餐厅
+    actlist: [],  //热门活动
+    hotlive: [],  //热门直播
+    food: [],   //美食墙
     logs: []
   },
   onLoad: function (options) {
-    let lat= '', lng= '';  //lat纬度   lng经度
+    let lat = '', lng = '';  //lat纬度   lng经度
     wx.getLocation({  //获取当前的地理位置
       type: 'wgs84',
       success: (res) => {
@@ -47,11 +47,11 @@ Page({
       }
     })
     let that = this;
-    setTimeout(function(){
-      if(lat && lng){
+    setTimeout(function () {
+      if (lat && lng) {
         that.requestCityName(lat, lng)
       }
-    },500)
+    }, 500)
   },
   requestCityName(lat, lng) {//获取当前城市
     wx.request({
@@ -76,14 +76,14 @@ Page({
     })
   },
   getdata: function () { // 获取推荐餐厅数据
-    Api.shoptop().then((res) => { 
+    Api.shoptop().then((res) => {
       // console.log("businss:",res.data.data)
-    this.setData({
+      this.setData({
         business: res.data.data
       })
     })
-  }, 
-  gettopic:function(){  // 美食墙
+  },
+  gettopic: function () {  // 美食墙
     Api.topictop().then((res) => {
       // console.log("food:", res.data.data)
       this.setData({
@@ -91,7 +91,7 @@ Page({
       })
     })
   },
-  getactlist(){  //获取热门活动数据
+  getactlist() {  //获取热门活动数据
     Api.actlist().then((res) => {
       // console.log("actlist:",res.data.data.list)
       this.setData({
@@ -99,7 +99,7 @@ Page({
       })
     })
   },
-  gethotlive(){  //获取热门直播数据 
+  gethotlive() {  //获取热门直播数据 
     let that = this;
     wx.request({
       url: that.data._build_url + 'zb/top/',
@@ -142,7 +142,7 @@ Page({
     })
   },
 
-//未开放 酒店 景点 休闲娱乐
+  //未开放 酒店 景点 休闲娱乐
   hotelUnopen: function () {
     wx.showToast({
       title: '该功能更新中...',
@@ -155,8 +155,8 @@ Page({
     })
   },
   entertainment: function () {
-    wx.showToast({
-      title: '该功能更新中...',
+    wx.navigateTo({
+      url: 'webview/webview',
     })
   },
   recommendCt: function (event) {
@@ -177,7 +177,7 @@ Page({
   },
   diningHhall: function (event) {  //跳转到商家（餐厅）内页
     const shopid = event.currentTarget.id
-    console.log("diningHhall:",shopid)
+    console.log("diningHhall:", shopid)
     wx.navigateTo({
       url: 'merchant-particulars/merchant-particulars?shopid=' + shopid
     })
@@ -186,7 +186,7 @@ Page({
     const id = e.currentTarget.id
     wx.navigateTo({
       // url:'../discover-plate/dynamic-state/dynamic-state'
-      url: '../discover-plate/dynamic-state/article_details/article_details?id='+id,
+      url: '../discover-plate/dynamic-state/article_details/article_details?id=' + id,
     })
     // wx.navigateTo({
     //   url: 'test?sfoodid=' + sfoodid
@@ -199,7 +199,7 @@ Page({
       url: '../activityDetails/details-like/details-like?actid=' + actid,
     })
   },
-  tolive:function(ev){ //跳转到直播内页
+  tolive: function (ev) { //跳转到直播内页
     const liveid = ev.currentTarget.id
     // console.log("liveid:",liveid)
     // wx.navigateTo({
--- utils/config/api.js
+++ utils/config/api.js
@@ -6,10 +6,10 @@ const ALL_API = {
   userlist: { //openId登录
     method: 'POST',  //请求方式  POST OR GET OR 其他请方式
     url: 'user/list'  //请求地址   完整地址是：http://www.hbxq001.cn/user/list    其中http://www.hbxq001.cn/已经在utils/config/config.js中配置，如果更换了后台地址或服务器，可直接到到config.js中统一更换，此处url只写后面部分
-  }, 
-  usersignup:{
-    method:'POST',
-    url:'user/signup'
+  },
+  usersignup: {
+    method: 'POST',
+    url: 'user/signup'
   },
   shoptop: {   //分页查询热门商家列表   推荐餐厅
     method: 'GET',
@@ -31,13 +31,13 @@ const ALL_API = {
     method: 'GET',
     url: 'act/top'
   },
-  hcllist:{  //首页轮播图
-    method:'GET',
-    url:'hcl/list'
+  hcllist: {  //首页轮播图
+    method: 'GET',
+    url: 'hcl/list'
   },
   topictop: { //首页美食墙
-    method:'GET',
-    url:'topic/top'
+    method: 'GET',
+    url: 'topic/top'
   },
   topiclist: {   //主题列表  
     method: 'GET',
@@ -48,9 +48,9 @@ const ALL_API = {
     url: 'act/get'
   },
 
-  actshoplist:{  //查询参加某一活动的商家
-    method:'GET',
-    url:'actshop/list'
+  actshoplist: {  //查询参加某一活动的商家
+    method: 'GET',
+    url: 'actshop/list'
   },
   actshoptop: {  //根据编号或名称搜索查询参加某一活动的商家某个商家
     method: 'GET',
@@ -63,35 +63,20 @@ const ALL_API = {
   terraceRoll: {   //享七劵列表
     method: 'GET',
     url: 'sku/list'
-  }, 
+  },
   paymentPay: {   //享七劵列表
     method: 'POST',
     url: '/wxpay/pay'
   },
-  voteadd:{  //投票
-    method:'POST',
-    url:'vote/add'
-  },
-  topicadd:{  //文章列表
-    method:'POST',
-    url:'topic/add'
-  },
-  cmtlist: {  //查询文章评论
-    method:'GET',
-    url:'cmt/list'
-  },
-  cmtadd:{   //新增文章评论
-    method:'POST',
-    url:'cmt/add'
-  },
-  zanadd:{   //点赞
-    method:'POST',
-    url:'zan/add'
-  },
-  zandelete:{   //取消点赞
+  voteadd: {  //投票
     method: 'POST',
-    url: 'zan/delete'
+    url: 'vote/add'
   },
+  //支付
+  topicadd: {
+    method: 'POST',
+    url: 'topic/add'
+  }
 }
 
 const Api = new MyHttp({}, ALL_API);
