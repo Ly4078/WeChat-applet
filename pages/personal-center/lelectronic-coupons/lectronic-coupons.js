2d670ddf2987b8c1a026aa6061eb8fc1f43ea3dc
yangfeng <1138391635@qq.com>
2018-03-16 22:19:03 +0800

    订单详情页面

--- pages/personal-center/lelectronic-coupons/lectronic-coupons.js
+++ pages/personal-center/lelectronic-coupons/lectronic-coupons.js
@@ -4,23 +4,69 @@ var app = getApp();
 Page({
   data: {
     _build_url: GLOBAL_API_DOMAIN,
-    userId: app.globalData.userInfo.userId ? app.globalData.userInfo.userId : 1     //登录用户的id
+    userId: app.globalData.userInfo.userId ? app.globalData.userInfo.userId : 1  ,   //登录用户的id
+    posts_key:[
+      {
+      aa: '01',
+      bb: '12345678',
+      },
+      {
+        aa: '02',
+        bb: '12345678',
+      },
+      {
+        aa: '03',
+        bb: '12345678',
+      }, 
+      {
+        aa: '04',
+        bb: '12345678',
+      }
+    ],
+    imgArr: [
+      'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=373481702,2170748029&fm=27&gp=0.jpg'
+    ]
   },
   onLoad: function (options) {
+    //卷码分格
+    let _data = this.data.posts_key
+    for (let i = 0; i < _data.length;i++){
+      let ncard = ''
+      for (var n = 0; n < _data[i].bb.length; n = n + 4) {
+        ncard += _data[i].bb.substring(n, n + 4) + " ";
+      }
+      _data[i].bb = ncard.replace(/(\s*$)/g, "")
+      console.log('分4位数',ncard.replace(/(\s*$)/g, ""));
+    }
     this.setData({
-      ticketId: options.id
+      ticketId: options.id,
+      posts_key:_data
     });
     this.getTicketInfo();
     this.getTel();
   },
+  
+  //二维码放大
+  previewImg: function (e) {
+    console.log(e.currentTarget.dataset.index);
+    var index = e.currentTarget.dataset.index;
+    var imgArr = this.data.imgArr;
+    wx.previewImage({
+      current: imgArr[index],     //当前图片地址
+      urls: imgArr,               //所有要预览的图片的地址集合 数组形式
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
   //获取票券详情
-  getTicketInfo: function() {
+  getTicketInfo: function () {
     let that = this;
     wx.request({
       url: that.data._build_url + 'cp/get/' + that.data.ticketId,
-      success: function(res) {
+      success: function (res) {
         let data = res.data;
-        if(data.code == 0) {
+        if (data.code == 0) {
           console.log(data.data)
           that.setData({
             ticketInfo: data.data,
@@ -32,10 +78,10 @@ Page({
     });
   },
   //获取订单信息
-  getOrderInfo: function() {
+  getOrderInfo: function () {
     let that = this;
     wx.request({
-      url: that.data._build_url + 'so/get/' + that.data.soId, 
+      url: that.data._build_url + 'so/get/' + that.data.soId,
       success: function (res) {
         let data = res.data;
         if (data.code == 0) {
@@ -48,12 +94,12 @@ Page({
     })
   },
   //获取手机号
-  getTel: function() {
+  getTel: function () {
     let that = this;
     wx.request({
       url: that.data._build_url + 'user/get/' + that.data.userId,
-      success: function(res) {
-        if(res.data.data) {
+      success: function (res) {
+        if (res.data.data) {
           that.setData({
             mobile: res.data.data.mobile
           });
@@ -61,7 +107,7 @@ Page({
       }
     })
   },
-  sublevelSum:function(event){
+  sublevelSum: function (event) {
     let that = this;
     wx.navigateTo({
       url: '../../index/voucher-details/voucher-details?id=' + that.data.ticketId + '&sell=' + that.data.orderInfo.soAmount + '&inp=' + that.data.ticketInfo.couponAmount + '&rule=' + that.data.ticketInfo.promotionRules[0].ruleDesc
