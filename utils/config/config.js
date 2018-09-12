10deddb041e7d1350ff5111f429a82870529f959
luyong <13971489895@163.com>
2018-09-12 13:53:06 +0800

    '1'

--- utils/config/config.js
+++ utils/config/config.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
- var BASEURL = "https://www.hbxq001.cn/";  //正式服务器
-=======
+
 
 var BASEURL = "https://www.hbxq001.cn/";  //正式服务器
->>>>>>> b5f65f799c51b4d303cbb89af6868c07063cf98b
+
 // var BASEURL =  "https://www.xq0036.top/";  //测试服务器
 
 var config = {
