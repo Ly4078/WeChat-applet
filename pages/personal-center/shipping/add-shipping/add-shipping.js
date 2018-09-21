45e7133d205337de9bc0410fe14108111b8827ef
webUserBaby <706353323@qq.com>
2018-09-22 00:08:09 +0800

    花花修改颜色

--- pages/personal-center/shipping/add-shipping/add-shipping.js
+++ pages/personal-center/shipping/add-shipping/add-shipping.js
@@ -393,7 +393,9 @@ Page({
             }
           })
         } else if (res.cancel) {
-          console.log('用户点击取消')
+          that.setData({
+            isremove: false
+          });
         }
       }
     })
