ad325d7b3656f2e326ba0cf1d70c0d6335344f12
yangfeng <Colin520vip@163.com>
2018-11-21 17:22:17 +0800

    更换名字

--- utils/config/config.js
+++ utils/config/config.js
@@ -3,9 +3,9 @@
 
 // var BASEURL =  "https://www.xq0036.top/";  //测试服务器
 
-var BASEURL =  "http://120.78.70.187:9081/";  //JWT服务器1
+// var BASEURL =  "http://120.78.70.187:9081/";  //JWT服务器1
 
-// var BASEURL =  "https://www.xiang7.net/";  //JWT正式服务器2
+var BASEURL =  "https://www.xiang7.net/";  //JWT正式服务器2
 var config = {
   GLOBAL_API_DOMAIN: BASEURL
 };
