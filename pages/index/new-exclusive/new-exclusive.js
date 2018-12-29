10c5212b292cdb53e64ea682182736752f515d9e
luyong <13971489895@163.com>
2018-12-29 15:39:00 +0800

    新用户领取

--- pages/index/new-exclusive/new-exclusive.js
+++ pages/index/new-exclusive/new-exclusive.js
@@ -6,7 +6,7 @@ let app = getApp()
 Page({
   data: {
     _build_url: GLOBAL_API_DOMAIN,
-    isNew: 0
+    isNew: 0  //0新用户  其他老用户
   },
   onShow:function(){
     this.isNewUser();
@@ -14,18 +14,19 @@ Page({
   isNewUser: function () {   //判断是否新用户
     let that = this;
     wx.request({
-      url: that.data._build_url + 'sku/isNewUser?userId=' + app.globalData.userInfo.userId,
+      url: that.data._build_url + 'orderInfo/isNewUser?userId=' + app.globalData.userInfo.userId,
       header: {
         "Authorization": app.globalData.token
       },
       success: function (res) {
+        console.log('res:',res)
         if (res.data.code == 0) {
           that.setData({
-            isNew: 1
+            isNew: 0
           });
         }else{
           that.setData({
-            isNew: 0
+            isNew: 1
           });
         }
       }
@@ -33,10 +34,10 @@ Page({
   },
   toReceive: function() {  //点击领取新人专享
     if (app.globalData.userInfo.mobile){
-      if (this.data.isNew == 1) {
+      if (this.data.isNew == 0) {
         let that = this, _parms = {};
         wx.request({
-          url: that.data._build_url + 'so/freeOrder?payType=2&skuId=8&skuNum=1',
+          url: that.data._build_url + 'orderInfo/createNewForFreer?payType=0&shopId=0&singleType=1&flagType=1',
           header: {
             "Authorization": app.globalData.token
           },
@@ -54,7 +55,7 @@ Page({
             }
           }
         })
-      } else if (this.data.isNew == 0) {
+      } else if (this.data.isNew == 1) {
         wx.showToast({
           title: '您已不是新用户！',
           icon: 'none'
